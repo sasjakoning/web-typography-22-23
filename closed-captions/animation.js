59d0ccf58dee05d389e1e2b892ffba1492f3e7f4
Sasja Koning <sasjakoning@hotmail.com>
2022-10-24 11:43:37 +0200

    added: svg filter animations

--- closed-captions/animation.js
+++ closed-captions/animation.js
@@ -1,25 +1,36 @@
-// const bodyElement = document.querySelector("body")
-// console.log(bodyElement)
-// const options = {
-//   attributes: true
-// }
+const bodyElement = document.querySelector("body")
+console.log(bodyElement)
+const options = {
+  attributes: true
+}
 
-// function callback(mutationList, observer) {
-//   mutationList.forEach(function(mutation) {
-//     if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-//       if(bodyElement.classList == "sound8"){
-//         console.log(" setting timeout")
-//         setTimeout(() => {
-//             console.log(" timeout done")
-//             document.getElementById("filter1").setAttribute("numOctaves", "10");
-//             document.getElementById("filter2").setAttribute("numOctaves", "10");
-//             document.getElementById("filter3").setAttribute("numOctaves", "10");
-//             document.getElementById("filter4").setAttribute("numOctaves", "10");
-//         }, 5000);
-//       }
-//     }
-//   })
-// }
 
-// const observer = new MutationObserver(callback)
-// observer.observe(bodyElement, options)
+
+function callback(mutationList, observer) {
+  mutationList.forEach(function(mutation) {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+      if(bodyElement.classList.value.includes('sound8') || bodyElement.classList.value.length == 7){
+        console.log(" setting timeout")
+        let baseVal = 1
+        let scaleVal = 5
+
+        setInterval(() => {
+            document.getElementById("filter1").children[0].setAttribute("baseFrequency", `0.0${baseVal}`)
+            document.getElementById("filter2").children[0].setAttribute("baseFrequency", `0.0${baseVal}`)
+            document.getElementById("filter3").children[0].setAttribute("baseFrequency", `0.0${baseVal}`)
+            document.getElementById("filter4").children[0].setAttribute("baseFrequency", `0.0${baseVal}`)
+            
+            document.getElementById("filter1").children[1].setAttribute("scale", `${scaleVal}`)
+            document.getElementById("filter2").children[1].setAttribute("scale", `${scaleVal}`)
+            document.getElementById("filter3").children[1].setAttribute("scale", `${scaleVal}`)
+            document.getElementById("filter4").children[1].setAttribute("scale", `${scaleVal}`)
+            baseVal++
+            scaleVal++
+        }, 5000);
+      }
+    }
+  })
+}
+
+const observer = new MutationObserver(callback)
+observer.observe(bodyElement, options)
